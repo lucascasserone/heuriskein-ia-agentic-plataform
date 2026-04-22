b840c00b318177d189abb627023b323e79204dc9
Lucas Casserone <marcolucascasserone@gmail.com>
2026-04-22 10:03:53 -0300

    hotfix: reverter PAGE_SIZE para 100 (500 error no Render)

--- backend/heuriskein/settings.py
+++ backend/heuriskein/settings.py
@@ -132,7 +132,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 100,
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.SearchFilter',
